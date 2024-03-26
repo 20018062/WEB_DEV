2dc80c9922a76ff3c41996374dd52138fc9a3e5a
20018062 <20018062@mydbs.ie>
2024-03-26 12:14:02 +0530

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from ecom import views
 from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -55,4 +56,8 @@ urlpatterns = [
 
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+"""from django.conf.urls.static import static from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)"""
