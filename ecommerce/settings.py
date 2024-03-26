390847962d342c293e6158769413cce9c4bd48ef
20018062 <20018062@mydbs.ie>
2024-03-26 12:29:42 +0530

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,10 +128,13 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS=[STATIC_DIR,]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/media/' 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL='/afterlogin'
